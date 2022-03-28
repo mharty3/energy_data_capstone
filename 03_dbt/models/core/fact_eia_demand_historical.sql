b85c709e915ba090e3005c2b1a1d1ed63130118c
Michael Harty <michaelharty3@gmail.com>
2022-03-28 03:15:40 +0000

    first dbt models

---
+++ 03_dbt/models/core/fact_eia_demand_historical.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='table') }}
+
+select 
+
+d.series_id,
+d.timestamp,
+d.value,
+md.units,
+md.name,
+md.updated
+		
+from {{ source('staging', 'demand_data_native') }} d 
+join {{ source('staging', 'demand_metadata_native') }} md
+on d.series_id = md.series_id
+
+where d.series_id = 'EBA_PSCO-ALL_D_H'
+order by d.timestamp
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
