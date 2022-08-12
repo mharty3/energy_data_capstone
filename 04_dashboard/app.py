1063713902fcac31ecf3fa3d6a498aeb458b4342
mharty3 <michaelharty3@gmail.com>
2022-08-12 22:56:19 +0000

    update app to show model forecasts

--- 04_dashboard/app.py
+++ 04_dashboard/app.py
@@ -24,6 +24,7 @@ client = bigquery.Client(credentials=credentials)
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "mlops-zoomcamp-354700")
 BIGQUERY_DATASET = 'energy_data_prod'
+MODEL_VERSION = "49c833f911ae43488e67063f410b7b5e"
 
 
 # Perform query.
@@ -37,17 +38,17 @@ def run_query(query):
     return pd.read_gbq(query, project_id=PROJECT_ID, credentials=credentials)
 
 
-def plot_demand_time_series(forecast_demand, actual_demand, weather_2022):
+def plot_demand_time_series(eia_forecast_demand, prod_model_demand, actual_demand, weather_2022):
 
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True)
 
     fig.add_traces(
         list(px.line(
-            forecast_demand, 
+            eia_forecast_demand, 
             x='timestamp_MTN', 
             y='value',
             title="Actual and Forecasted Electrical Demand, Xcel Energy, Colorado",
-            labels={'value': 'Demand (megawatthours)'}
+            labels={'value': 'EIA Forecasted Demand (megawatthours)'}
             )
             .select_traces()
         )
@@ -56,6 +57,8 @@ def plot_demand_time_series(forecast_demand, actual_demand, weather_2022):
     fig.add_traces(
         list(px.line(actual_demand, x='timestamp_MTN', y='value', labels={'value': 'Actual_Demand (megawatthours)'}).select_traces())
         )
+
+    fig.add_traces(px.scatter(prod_model_demand, x='timestamp_MTN', y='value', labels={'value': 'Predicted_Demand (megawatthours)'}).select_traces())
 
     fig.add_trace(
         list(px.scatter(weather_2022, x='observation_time_MTN', y='temp_F').select_traces())[0],
@@ -93,12 +96,21 @@ def main():
                                     date(timestamp_MTN) BETWEEN date('{start_date}') and date('{end_date}') 
                                   ORDER BY timestamp_MTN""")
 
-    forecast_demand = run_query(f"""SELECT * 
-                                    FROM 
-                                      `{PROJECT_ID}.{BIGQUERY_DATASET}.fact_eia_demand_forecast` 
-                                    WHERE 
-                                      date(timestamp_MTN) BETWEEN date('{start_date}') and date('{end_date}')
-                                    ORDER BY timestamp_MTN""")
+    eia_forecast_demand = run_query(f"""SELECT * 
+                                        FROM 
+                                          `{PROJECT_ID}.{BIGQUERY_DATASET}.fact_eia_demand_forecast` 
+                                        WHERE 
+                                          date(timestamp_MTN) BETWEEN date('{start_date}') and date('{end_date}')
+                                        ORDER BY timestamp_MTN""")
+
+    prod_model_demand = run_query(f"""SELECT 
+                                        DATETIME(energy_timestamp_mtn, "America/Denver") as timestamp_MTN,
+                                        predicted_energy_demand,
+                                        temp_f_forecast
+                                  FROM `{PROJECT_ID}.{BIGQUERY_DATASET}.energy_demand_forecasts` 
+                                  WHERE model_version = '{MODEL_VERSION}'
+                                  """
+
     
     weather_2022 = run_query(f"""SELECT * 
                                FROM 
@@ -107,7 +119,7 @@ def main():
                                  observation_time_MTN BETWEEN date('{start_date}') and date('{end_date}')
                                ORDER BY observation_time_MTN""")
 
-    fig = plot_demand_time_series(forecast_demand, actual_demand, weather_2022)
+    fig = plot_demand_time_series(eia_forecast_demand, actual_demand, weather_2022)
     fig.update_layout(height=700)
     st.plotly_chart(fig, use_container_width=True)
 
@@ -115,8 +127,8 @@ if __name__ == '__main__':
 
     st.title('⚡ Energy Demand and Temperature for Xcel Energy in CO ⚡')
     st.sidebar.write(info_text)
-    with st.sidebar.expander('Note on missing data between May 20 and May 30, 2022:'):
-      st.write(note)
+    # with st.sidebar.expander('Note on missing data between May 20 and May 30, 2022:'):
+    #   st.write(note)
 
     main()
 
--- 04_dashboard/info.py
+++ 04_dashboard/info.py
@@ -4,13 +4,12 @@ Michael Harty
 
 [Project Repo](https://github.com/mharty3/energy_data_capstone)
 
-I made this dashboard as part of my Data Engineering Zoomcamp Capstone Project. 
+I made this dashboard as part of my Data Engineering and MLOps Zoomcamp Capstone Project. 
 
 It displays data from the data pipeline I created that extracts and transforms data from various sources including the EIA, NOAA, and Open Weather Map API.
 
 Actual energy demand and weather data is updated hourly, and the EIA energy demand forecast is updated each morning whenever the EIA releases their forecast for that day (usually around 8am MDT).
 
-The data is stored in a data warehouse using Google Big Query with the idea that a data scientist (future me) will be able to use it to train a new forecast model and compare it to the EIA's forecast.
 
 """
 
